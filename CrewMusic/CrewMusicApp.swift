de236430ac0925574692802ea375559b20099a80
Eugenio Militerno <82472181+BadBro00@users.noreply.github.com>
2023-01-05 11:19:21 +0100

    CrewMusicApp

---
+++ CrewMusic/CrewMusicApp.swift
@@ -0,0 +1,18 @@
+//
+//  CrewMusicApp.swift
+//  CrewMusic
+//
+//  Created by Ipavon on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct CrewMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
