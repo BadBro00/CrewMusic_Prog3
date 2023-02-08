503d9a8e2c11d38b08f852a3ba0f33927d6e691c
Eugenio Militerno <82472181+BadBro00@users.noreply.github.com>
2023-02-08 20:23:41 +0100

    Update Home.java

--- JAVA/Home.java
+++ JAVA/Home.java
@@ -1,19 +1,10 @@
 package JAVA;
-
 import java.util.ArrayList;
 
 public class Home{
-    public static void main(String args[]) {
-        Canzone A = new Canzone("a","a","a");
-        Canzone B = new Canzone("b","b","b");
-        Canzone C = new Canzone("c","c","c");
-        ArrayList<Canzone> arrayofSongs = new ArrayList<Canzone>();
-        arrayofSongs.add(A);
-        arrayofSongs.add(B);
-        arrayofSongs.add(C);
-
-        for(Canzone song : arrayofSongs){
-            System.out.println("CANZONE: " + song.getTitle() + ", ARTISTA: " + song.getArtist() + ", DALL'ALBUM: " + song.getAlbum() + "\n");
-        }
+    public static void main(String args[]){
+       retriever R = new retriever();
+       System.out.println("Home");
+       //R.Retriever();
     }
 }
